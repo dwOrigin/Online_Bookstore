05151ec301b0d6463ebf4988ad958c4458dc4d7e
gj <864066131@qq.com>
2023-02-03 13:46:41 +0800

    修改

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import '@/assets/css/global.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    
+    home
   </div>
 </template>
 
