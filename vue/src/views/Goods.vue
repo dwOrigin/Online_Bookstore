e69d93f7c78bd44118395bbf795568cbd622470b
gj <864066131@qq.com>
2023-02-16 19:05:21 +0800

    点击进去

--- vue/src/views/Goods.vue
+++ vue/src/views/Goods.vue
@@ -4,11 +4,11 @@
     <el-row :gutter="10">
         <el-col :span = "6" v-for ="item in goods" :key = "item.id" style="margin-bottom:10px">
         <div>
-            <img :src="item.img" alt="" style="width :100%;height: 200px;overflow: hidden; border-radius: 10px 10px 0 0">
+            <img :src="item.img" alt="" style="width :100%;height: 200px;overflow: hidden; border-radius: 10px 10px 0 0" @click="seeDetail()">
             <div style="color: #666; padding: 10px">{{item.name}}</div>
             <div style="padding: 10px">
             <el-button type="primary">购买</el-button>
-            <el-button type="success" @click="seeDetail()">查看</el-button>
+            
             </div>
         </div>
         </el-col>
